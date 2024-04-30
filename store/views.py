c6e0434cfd9f0e85b65cbb958f7ced0d5b712c04
Prodosh <duttaprodosh@gmail.com>
2024-04-30 21:30:36 +0530

    Cart Persistance

--- store/views.py
+++ store/views.py
@@ -8,6 +8,52 @@ from .forms import SignUpForm, UserInfoForm, UpdateUserForm, ChangePasswordForm
 
 from django import forms
 
+from django import forms
+from django.db.models import Q
+import json
+from cart.cart import Cart
+
+def search(request):
+	# Determine if they filled out the form
+	if request.method == "POST":
+		searched = request.POST['searched']
+		# Query The Products DB Model
+		searched = Product.objects.filter(Q(name__icontains=searched) | Q(description__icontains=searched))
+		# Test for null
+		if not searched:
+			messages.success(request, "That Product Does Not Exist...Please try Again.")
+			return render(request, "search.html", {})
+		else:
+			return render(request, "search.html", {'searched':searched})
+	else:
+		return render(request, "search.html", {})
+
+def update_info(request):
+    if request.user.is_authenticated:
+        # Get Current User
+        current_user = Profile.objects.get(user__id=request.user.id)
+        # Get Current User's Shipping Info
+#        shipping_user = ShippingAddress.objects.get(user__id=request.user.id)
+
+        # Get original User Form
+        form = UserInfoForm(request.POST or None, instance=current_user)
+        # Get User's Shipping Form
+#        shipping_form = ShippingForm(request.POST or None, instance=shipping_user)
+        if form.is_valid():
+                # or shipping_form.is_valid():
+            # Save original form
+            form.save()
+            # Save shipping form
+            #shipping_form.save()
+
+            messages.success(request, "Your Info Has Been Updated!!")
+            return redirect('home')
+        return render(request, "update_info.html", {'form': form})
+    else:
+        messages.success(request, "You Must Be Logged In To Access That Page!!")
+        return redirect('home')
+
+
 def home(request):
     products = Product.objects.all()
     return render(request, 'home.html', {'products':products})
@@ -22,6 +68,22 @@ def login_user(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
+
+            # Do some shopping cart stuff
+            current_user = Profile.objects.get(user__id=request.user.id)
+            # Get their saved cart from database
+            saved_cart = current_user.old_cart
+            # Convert database string to python dictionary
+            if saved_cart:
+                # Convert to dictionary using JSON
+                converted_cart = json.loads(saved_cart)
+                # Add the loaded cart dictionary to our session
+                # Get the cart
+                cart = Cart(request)
+                # Loop thru the cart and add the items from the database
+                for key, value in converted_cart.items():
+                    cart.db_add(product=key, quantity=value)
+
             messages.success(request, ("You Have Been Logged In!"))
             return redirect('home')
         else:
@@ -47,7 +109,7 @@ def register_user(request):
             user = authenticate(username=username, password=password)
             login(request, user)
             messages.success(request, ("Username Created - Please Fill Out Your User Info Below..."))
-            return redirect('home')
+            return redirect('update_info')
         else:
             messages.success(request, ("Whoops! There was a problem Registering, please try again..."))
             return redirect('register')

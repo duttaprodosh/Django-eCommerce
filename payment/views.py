cb55353e4d860a62d7fbf21e5e9780ad95cbe0dc
Prodosh <duttaprodosh@gmail.com>
2024-05-15 12:33:53 +0530

    Final Commit

--- payment/views.py
+++ payment/views.py
@@ -4,7 +4,7 @@ from payment.forms import ShippingForm, PaymentForm
 from payment.models import ShippingAddress, Order, OrderItem
 from django.contrib.auth.models import User
 from django.contrib import messages
-from store.models import Product
+from store.models import Product, Profile
 
 def payment_success(request):
 	return render(request, "payment/payment_success.html", {})
@@ -84,6 +84,118 @@ def billing_info(request):
 		return render(request, "payment/billing_info.html",
 					  {"cart_products": cart_products, "quantities": quantities, "totals": totals,
 					   "shipping_form": shipping_form})
+	else:
+		messages.success(request, "Access Denied")
+		return redirect('home')
+
+
+def process_order(request):
+	if request.POST:
+		# Get the cart
+		cart = Cart(request)
+		cart_products = cart.get_prods
+		quantities = cart.get_quants
+		totals = cart.cart_total()
+
+		# Get Billing Info from the last page
+		payment_form = PaymentForm(request.POST or None)
+		# Get Shipping Session Data
+		my_shipping = request.session.get('my_shipping')
+
+		# Gather Order Info
+		full_name = my_shipping['shipping_full_name']
+		email = my_shipping['shipping_email']
+		# Create Shipping Address from session info
+		shipping_address = f"{my_shipping['shipping_address1']}\n{my_shipping['shipping_address2']}\n{my_shipping['shipping_city']}\n{my_shipping['shipping_state']}\n{my_shipping['shipping_zipcode']}\n{my_shipping['shipping_country']}"
+		amount_paid = totals
+
+		# Create an Order
+		if request.user.is_authenticated:
+			# logged in
+			user = request.user
+			# Create Order
+			create_order = Order(user=user, full_name=full_name, email=email, shipping_address=shipping_address,
+								 amount_paid=amount_paid)
+			create_order.save()
+
+			# Add order items
+
+			# Get the order ID
+			order_id = create_order.pk
+
+			# Get product Info
+			for product in cart_products():
+				# Get product ID
+				product_id = product.id
+				# Get product price
+				if product.is_sale:
+					price = product.sale_price
+				else:
+					price = product.price
+
+				# Get quantity
+				for key, value in quantities().items():
+					if int(key) == product.id:
+						# Create order item
+						create_order_item = OrderItem(order_id=order_id, product_id=product_id, user=user,
+													  quantity=value, price=price)
+						create_order_item.save()
+
+			# Delete our cart
+			for key in list(request.session.keys()):
+				if key == "session_key":
+					# Delete the key
+					del request.session[key]
+
+			# Delete old cart from Profile of the User
+			current_user = Profile.objects.filter(user__id=request.user.id)
+			current_user.update(old_cart=None)
+
+			messages.success(request, "Order Placed!")
+			return redirect('home')
+
+
+
+		else:
+			# not logged in
+			# Create Order
+			create_order = Order(full_name=full_name, email=email, shipping_address=shipping_address,
+								 amount_paid=amount_paid)
+			create_order.save()
+
+			# Add order items
+
+			# Get the order ID
+			order_id = create_order.pk
+
+			# Get product Info
+			for product in cart_products():
+				# Get product ID
+				product_id = product.id
+				# Get product price
+				if product.is_sale:
+					price = product.sale_price
+				else:
+					price = product.price
+
+				# Get quantity
+				for key, value in quantities().items():
+					if int(key) == product.id:
+						# Create order item
+						create_order_item = OrderItem(order_id=order_id, product_id=product_id, quantity=value,
+													  price=price)
+						create_order_item.save()
+
+			# Delete our cart
+			for key in list(request.session.keys()):
+				if key == "session_key":
+					# Delete the key
+					del request.session[key]
+
+			messages.success(request, "Order Placed!")
+			return redirect('home')
+
+
 	else:
 		messages.success(request, "Access Denied")
 		return redirect('home')

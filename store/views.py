0426ab37cdbe40c57b9d71371cc67a30732ce569
Prodosh <duttaprodosh@gmail.com>
2024-05-16 17:51:18 +0530

    Search in Home Page

--- store/views.py
+++ store/views.py
@@ -15,19 +15,21 @@ import json
 from cart.cart import Cart
 
 def search(request):
+
 	# Determine if they filled out the form
-	if request.method == "POST":
-		searched = request.POST['searched']
-		# Query The Products DB Model
-		searched = Product.objects.filter(Q(name__icontains=searched) | Q(description__icontains=searched))
-		# Test for null
-		if not searched:
-			messages.success(request, "That Product Does Not Exist...Please try Again.")
-			return render(request, "search.html", {})
-		else:
-			return render(request, "search.html", {'searched':searched})
-	else:
-		return render(request, "search.html", {})
+    if request.method == "POST":
+        searched = request.POST['searched']
+        # Query The Products DB Model
+        category = Category.objects.filter(Q(name__icontains=searched))
+        products = Product.objects.filter(Q(name__icontains=searched) | Q(description__icontains=searched) | Q(category__in=category))
+        # Test for null
+        if not products:
+            messages.success(request, "That Product Does Not Exist...Please try Again.")
+            return render(request, "search.html", {})
+        else:
+            return render(request, "home.html", {'products':products,'searchvalue':request.POST['searched']})
+    else:
+        return render(request, "search.html", {})
 
 def update_info(request):
     if request.user.is_authenticated:
